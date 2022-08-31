38af0975ada56b0bf4aa07e3f0263a2372091350
Nick <nfears5798@gmail.com>
2022-08-31 00:33:47 -0400

    commit tree bf3712791da3cbfa540841ba36d78e01d720eeea

---
+++ index.js
@@ -0,0 +1,38 @@
+function add() {
+    return "something";
+  }
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(a) {
+    return a + 1
+}
+function decrement(a) {
+    return a - 1
+}
+function makeInt(n) {
+    n = parseInt(n, 10);
+    return n
+}
+function preserveDecimal(n) {
+    n = parseFloat(n);
+    return n;
+}
